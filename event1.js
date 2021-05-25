ea0a13392080bdc68bba0421ddf1d6ce3a9f9ce3
thanhnd3996 <ndt4081@gmail.com>
2021-05-26 03:37:58 +0700

    update

--- event1.js
+++ event1.js
@@ -1,26 +1,40 @@
+import EventEmitter from 'events';
 import fs from 'fs';
 
-export default class Event1 {
+export default class Event1 extends EventEmitter {
+    constructor() {
+        super();
+        this.on('ListEnabledEvent', (filepath) => {
+            let evt_list = this.browsing(filepath);
+            console.log(`list of enabled event: ${evt_list}`);
+        });
+    }
     browsing(file_path) {
+        let evt_list = [];
         fs.readFile(file_path, 'utf8', (err, data) => {
+            console.log('123');
+            // read EventInfo.json
             if (err) {
                 console.log(err);
             } else {
                 const events = JSON.parse(data);
                 Object.values(events).forEach((evt) => {
                     fs.readFile(evt.info, 'utf8', (err, data) => {
+                        //read event.info.json
                         if (err) {
                             console.log(err);
                         } else {
                             const info = JSON.parse(data);
-
-                            Object.values(info).forEach(() => {
-                                console.log(info.isEnabled);
-                            });
+                            if (info.isEnabled) {
+                                // fs.readFile(evt.handle, 'utf8', (err, handle));
+                                evt_list.push(info.name);
+                            }
                         }
                     });
                 });
             }
         });
+        console.log('456');
+        return evt_list;
     }
 }
