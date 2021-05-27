d29360733effe5f625450d7ba70f58577cea6506
thanhnd3996 <ndt4081@gmail.com>
2021-05-27 17:11:19 +0700

    task6

--- event1.js
+++ event1.js
@@ -9,32 +9,21 @@ export default class Event1 extends EventEmitter {
             console.log(`list of enabled event: ${evt_list}`);
         });
     }
+
     browsing(file_path) {
         let evt_list = [];
-        fs.readFile(file_path, 'utf8', (err, data) => {
-            console.log('123');
-            // read EventInfo.json
-            if (err) {
-                console.log(err);
-            } else {
-                const events = JSON.parse(data);
-                Object.values(events).forEach((evt) => {
-                    fs.readFile(evt.info, 'utf8', (err, data) => {
-                        //read event.info.json
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            const info = JSON.parse(data);
-                            if (info.isEnabled) {
-                                // fs.readFile(evt.handle, 'utf8', (err, handle));
-                                evt_list.push(info.name);
-                            }
-                        }
-                    });
-                });
+        let data1 = fs.readFileSync(file_path, 'utf8');
+        // read EventInfo.json
+        const events = JSON.parse(data1);
+        Object.values(events).forEach((evt) => {
+            let data2 = fs.readFileSync(evt.info, 'utf8');
+            //read event.info.json
+            const info = JSON.parse(data2);
+            if (info.isEnabled) {
+                // fs.readFile(evt.handle, 'utf8', (err, handle));
+                evt_list.push(info.name);
             }
         });
-        console.log('456');
         return evt_list;
     }
 }
