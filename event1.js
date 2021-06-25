e3a8026532b961926cf6a6ed5e4d8f0dd169806f
thanhnd3996 <ndt4081@gmail.com>
2021-06-25 17:27:46 +0700

    update

--- event1.js
+++ event1.js
@@ -11,19 +11,49 @@ export default class Event1 extends EventEmitter {
 
     browsing(file_path) {
         let evt_list = [];
-        let handle_file_list = [];
-        // read EventInfo.json
-        const events = JSON.parse(fs.readFileSync(file_path, 'utf8'));
-        Object.values(events).forEach((evt) => {
-            //read event.info.json
-            const info = JSON.parse(fs.readFileSync(evt.info, 'utf8'));
-            if (info.isEnabled) {
-                evt_list.push(info.name);
-                let data = fs.readFileSync(evt.handle, 'utf8');
-                handle_file_list.push(data);
+        fs.readFile(file_path, 'utf8', (err, data) => {
+            console.log('123');
+            // read EventInfo.json
+            if (err) {
+                console.log(err);
+            } else {
+                const events = JSON.parse(data);
+                Object.values(events).forEach((evt) => {
+                    fs.readFile(evt.info, 'utf8', (err, data) => {
+                        //read event.info.json
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            const info = JSON.parse(data);
+                            if (info.isEnabled) {
+                                // fs.readFile(evt.handle, 'utf8', (err, handle));
+                                evt_list.push(info.name);
+                            }
+                        }
+                    });
+                });
             }
         });
-        console.log(`list of enabled event: ${evt_list}`);
-        return handle_file_list;
+        console.log('456');
+
+        return evt_list;
     }
+
+    // browsing(file_path) {
+    //     let evt_list = [];
+    //     let handle_file_list = [];
+    //     // read EventInfo.json
+    //     const events = JSON.parse(fs.readFileSync(file_path, 'utf8'));
+    //     Object.values(events).forEach((evt) => {
+    //         //read event.info.json
+    //         const info = JSON.parse(fs.readFileSync(evt.info, 'utf8'));
+    //         if (info.isEnabled) {
+    //             evt_list.push(info.name);
+    //             let data = fs.readFileSync(evt.handle, 'utf8');
+    //             handle_file_list.push(data);
+    //         }
+    //     });
+    //     console.log(`list of enabled event: ${evt_list}`);
+    //     return handle_file_list;
+    // }
 }
